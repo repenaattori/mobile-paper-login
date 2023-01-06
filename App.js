c154c77c7f3fb46988a1692fc19c5de99428ca19
repenaattori <reimarii@oamk.fi>
2023-01-06 11:07:45 +0200

    init

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {  View} from 'react-native';
+import Styles from './styles/Styles.js';
+import {MD3LightTheme, Provider, Text, Button, TextInput} from 'react-native-paper';
+
+
+export default function App() {
+
+  return (
+   <></>
+  );
+}
+
