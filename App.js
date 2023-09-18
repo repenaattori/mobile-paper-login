410a1566abee92f96e89523982bd1f9689c16ea2
repsukka <81526112+repsukka@users.noreply.github.com>
2023-09-18 15:54:13 +0300

    Version and minor updates

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import {  View} from 'react-native';
 import Styles from './styles/Styles.js';
 import {MD3LightTheme, Provider, Text, Button, TextInput} from 'react-native-paper';
 
+const MyTheme = {
+}
 
 export default function App() {
 
   return (
-   <></>
+   <Provider theme={MyTheme }>
+      <View style={Styles.container}>
+      </View>
+   </Provider>
   );
 }
 
